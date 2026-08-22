3a0612b205a3b647a22ee186c9513a1aed3c7ba7
amine mkacher <mkacher.97@gmail.com>
2026-08-22 14:24:18 +0100

    cv update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.png";
-import CV_FR from "../../assets/AmineMkacher-CV-FR.pdf";
-import CV_EN from "../../assets/AmineMkacher-CV-EN.pdf";
+import CV_FR from "../../assets/AmineMkacher-Fr-Cv.pdf";
+import CV_EN from "../../assets/AmineMkacher-Eng-Cv.pdf";
 import Info from "./Info";
 import { useTranslation } from "react-i18next";
 import { motion } from "framer-motion";
